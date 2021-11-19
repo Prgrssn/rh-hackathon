0f97fcf6840b21ef370701fbc2ef370f6c4c9f2d
Joshua Young <prgrssn@gmail.com>
2021-11-18 22:37:15 -0800

    create json file with mock data and created backend api call logic

--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
+const axios = require("axios");
 PORT = 8080;
+
+API_URL = `http://localhost:${PORT}`;
+
+axios
+  .get(`${API_URL}/quiz`)
+  .then((res) => {
+    fs.readFile("./data/quiz.json", (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.JSON.parse(data);
+    });
+  })
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Express server running on port: ${PORT}`);

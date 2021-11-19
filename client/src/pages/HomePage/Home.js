65215b8776f5f6072f4614b0df8661b4767570dd
arvin361 <arvin_361@hotmail.com>
2021-11-19 07:14:20 -0500

    Finalize home page styling

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -3,6 +3,9 @@ import "./Home.scss";
 import landing from "../../assets/images/Landing visual@2x.png";
 import bitcoin from "../../assets/icons/Bitcoin@2x.png";
 import bitcoins from "../../assets/icons/5 Bitcoin@2x.png";
+import video from "../../assets/images/Video thumbnail@2x.png";
+import verified from "../../assets/icons/Checkmark@2x.png";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -40,6 +43,23 @@ export default function Home() {
             5 Cryptohood Credits can be cashed in for $5 in your account.
           </p>
         </div>
+      </section>
+      <section className="page__section4">
+        <h3 className="page__subtitle2">Start earning with the first video:</h3>
+        <Link to={`/quiz`}>
+          <img src={video} alt="Video Player" className="page__video" />
+        </Link>
+        <div className="page__verified">
+          <img
+            src={verified}
+            alt="Verified Icon"
+            className="page__verified-icon"
+          />
+          <p className="page__label">Verified by Robinhood</p>
+        </div>
+        <p className="page__footer">
+          1,870 Robinhooders successfully completed this lesson.
+        </p>
       </section>
     </div>
   );

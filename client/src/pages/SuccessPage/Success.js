890caac28b4fd49ac31500731a603669901f670c
arvin361 <arvin_361@hotmail.com>
2021-11-19 14:25:34 -0500

    Fix success

--- client/src/pages/SuccessPage/Success.js
+++ client/src/pages/SuccessPage/Success.js
@@ -1,12 +1,15 @@
 import "./Success.scss";
 import successImg from "../../assets/images/Success page@2x.png";
+import { Link } from "react-router-dom";
 
 export default function Success() {
   return (
     <main className="quiz">
       <article className="success-card">
         <div className="success-card__img-cont">
-          <img className="success-card__img" alt="success" src={successImg} />
+          <Link to={`/`}>
+            <img className="success-card__img" alt="success" src={successImg} />
+          </Link>
         </div>
       </article>
     </main>

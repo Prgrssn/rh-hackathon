84982f8b259c8f69354cd2ceffc622c0967e9edc
Joshua Young <prgrssn@gmail.com>
2021-11-18 21:50:50 -0800

    rough template of header component to be iterated upon

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello World!</h1>
     </div>
   );
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__icon-cont">
+        <img className="header__icon" />
+      </div>
+    </header>
+  );
+}

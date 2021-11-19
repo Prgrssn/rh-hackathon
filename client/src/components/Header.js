732ffe4e57eff6d58e87ceb817a29bcc7587799f
arvin361 <arvin_361@hotmail.com>
2021-11-19 07:42:38 -0500

    Make home page slightly responsive past 450px viewport width

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,12 +3,15 @@ import "./Header.scss";
 import nav from "../assets/icons/Hamburger menu@2x.png";
 import logo from "../assets/icons/Logo@2x.png";
 import profile from "../assets/icons/Profile icon@2x.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <img className="header__nav" src={nav} alt="Navigation" />
-      <img className="header__logo" src={logo} alt="Logo" />
+      <Link to={`/`}>
+        <img className="header__logo" src={logo} alt="Logo" />
+      </Link>
 
       <img className="header__profile" src={profile} alt="Profile" />
     </header>
